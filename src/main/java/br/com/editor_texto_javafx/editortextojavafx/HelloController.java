e58519977330c431ada6340a654ec16ff510d0b9
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2025-01-19 13:43:24 -0300

    next...

--- src/main/java/br/com/editor_texto_javafx/editortextojavafx/HelloController.java
+++ src/main/java/br/com/editor_texto_javafx/editortextojavafx/HelloController.java
@@ -3,8 +3,11 @@ package br.com.editor_texto_javafx.editortextojavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 public class HelloController {
     @FXML
@@ -20,16 +23,54 @@ public class HelloController {
         }
 
     }
-
-    private void salvarArquivoComo() {
-    }
-
     private void escreverAquivoComo(File file, String coteudo) {
 
-        try {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(coteudo);
 
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
+    private void salvarArquivoComo() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Salvar arquivo");
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivo de texto(*,.txt)", "*.txt"));
+
+        arquivoAtual = fileChooser.showSaveDialog(textArea.getScene().getWindow());
+
+        if (arquivoAtual != null) {
+            escreverAquivoComo(arquivoAtual, textArea.getText());
+        }
+    }
+        public void novoArquivo(){
+
+        }
+        public void abrirArquivo(){
+
+        }
+        public void copiar(){
+
+        }
+        public void colar(){
+
+        }
+        public void selecionar(){
+
+        }
+        public void modoClaro(){
+
+        }
+        public void modoEscuro() {
+
+        }
+        public void exibirInformacoes(){
+
+    }
+
+
+
 
 
 }
